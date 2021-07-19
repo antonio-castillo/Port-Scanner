1625494f65b778d6cefc41abd92c10d66728ea8a
antonio-castillo <a.castillomoreno92@gmail.com>
2021-07-19 12:57:29 +0200

    Bugfixes

--- Port Scanner/Form1.cs
+++ Port Scanner/Form1.cs
@@ -18,14 +18,17 @@ namespace Port_Scanner
             InitializeComponent();
         }
 
+        
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            progressBar1.Value = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            progressBar1.Value = 0;
             if (string.IsNullOrEmpty(textBox1.Text))
             {
                 MessageBox.Show("You must introduce an IP Address");
@@ -35,28 +38,30 @@ namespace Port_Scanner
                 listBox1.Items.Clear();
                 if (radioButton1.Checked)
                 {
-
+                    progressBar1.Visible = true;
+                    progressBar1.Maximum = 1024;
+                    progressBar1.Value = 0;
                     //Recoge la direccion ip del textbox1
                     string ip = textBox1.Text;
                     //65535
 
                     for (int actualPort = 0; actualPort < 1024; actualPort++)
                     {
+                        progressBar1.Step = 1;
+                        progressBar1.Visible = true;
+                        progressBar1.PerformStep();
 
                         try
                         {
                             TcpClient client = new TcpClient(ip, actualPort);
                             listBox1.Items.Add("IP: " + ip + "  PORT: " + actualPort + " STATUS: OPEN");
+                    
                         }
                         catch (Exception ex)
                         {
                             //listBox1.Items.Add("IP: " + ip + "  PORT: " + actualPort + "STATUS: CLOSED");
                         }
                     }
-
-
-
-
 
                 }
                 else if (radioButton2.Checked)
@@ -70,7 +75,7 @@ namespace Port_Scanner
                     else
                     {
 
-
+                        progressBar1.Maximum = 1;
                         //Recoge la direccion ip del textbox1
                         string ip = textBox1.Text;
 
@@ -82,10 +87,14 @@ namespace Port_Scanner
                         {
                             TcpClient client = new TcpClient(ip, ipPort);
                             listBox1.Items.Add("IP: " + ip + "  PORT: " + ipPort + "   STATUS: OPEN");
+                            progressBar1.Step = 1;
+                            progressBar1.PerformStep();
                         }
                         catch (Exception ex)
                         {
                             listBox1.Items.Add("IP: " + ip + "  PORT: " + ipPort + "   STATUS: CLOSED");
+                            progressBar1.Step = 1;
+                            progressBar1.PerformStep();
                         }
 
 
@@ -101,6 +110,13 @@ namespace Port_Scanner
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start("explorer.exe", "https://github.com/antonio-castillo");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            System.Environment.Exit(0);
+
         }
     }
 }
